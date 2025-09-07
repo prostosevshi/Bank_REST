3fb96bd3dc3037403a0d850d92d75eeba30329f7
pepela <pfuntusha@gmail.com>
2025-09-07 20:52:59 +0400

    add SwaggerUI

---
+++ src/main/java/com/example/bankcards/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package com.example.bankcards.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Bank REST API")
+                        .version("1.0.0")
+                        .description("REST API для работы с банковскими картами и переводами"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
